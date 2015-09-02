c048b1185a096d65bf57e928f03a20cf50267cab
Ronert Obst <ronert.obst@gmail.com>
2015-09-02 12:31:30 +0200

    fixed redis service

--- IoT-DataScience/PythonModel/StreamPredict.py
+++ IoT-DataScience/PythonModel/StreamPredict.py
@@ -18,12 +18,12 @@
 # Ipython StreamPredict.py Configuration/default.conf  TODO: replace with docopt
 
 import json
-import logging
 import os
-import StringIO
+import cPickle as pickle
 import sys
 
 import Data
+import logging
 import numpy as np
 import redis
 
@@ -31,7 +31,6 @@ from datetime import datetime
 from datetime import timedelta
 from pandas import Series
 from pandas import concat
-from sklearn.externals import joblib
 from toolz import dicttoolz
 
 
@@ -40,12 +39,10 @@ logger = logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 # Redis setup
 services = json.loads(os.getenv("VCAP_SERVICES"))
-redis_env = services["stream-cloud-streams-bus"][0]["credentials"]
+redis_env = services["p-redis"][0]["credentials"]
 raw_data_queue = os.getenv("raw_data")
 predictions_topic = os.getenv("predictions")
-model_table = os.getenv("model_table")
-redis_env['host'] = redis_env['hostname']
-del redis_env['hostname']
+model_db = os.getenv("model_db")
 redis_env['port'] = int(redis_env['port'])
 
 # Connect to Redis
@@ -66,20 +63,20 @@ cutoff = timedelta(minutes=int(os.getenv("cutoff")))
 
 
 # Load models
-init_models = joblib.load(StringIO.StringIO(model_table["init_models"]))
-online_models = joblib.load(StringIO.StringIO(model_table["online_models"]))
+init_models = pickle.loads(r.get("init_models"))
+online_models = pickle.loads(r.get("online_models"))
 
 # Load information from historical data
-journey_clusters = joblib.load(StringIO.StringIO(model_table["clusters"]))
+journey_clusters = pickle.loads(r.get("journey_clusters"))
 
 end_locations = dicttoolz.valmap(lambda x: [journey_cluster.averages[["EndLat", "EndLong"]].values.tolist() for
-				 journey_cluster in x if journey_cluster.clusterID != -1], journey_clusters)
+                                 journey_cluster in x if journey_cluster.clusterID != -1], journey_clusters)
 
 mpg_insts = dicttoolz.valmap(lambda x: [journey_cluster.averages["MPG_from_MAF"] if "MPG_from_MAF" in journey_cluster.averages.keys() else float("NaN") for
-					journey_cluster in x if journey_cluster.clusterID != -1], journey_clusters)
+                                        journey_cluster in x if journey_cluster.clusterID != -1], journey_clusters)
 
 cluster_ids = dicttoolz.valmap(lambda x: [str(journey_cluster.clusterID) for
-					  journey_cluster in x if journey_cluster.clusterID != -1], journey_clusters)
+                                          journey_cluster in x if journey_cluster.clusterID != -1], journey_clusters)
 
 journeys = {}
 initial_predictions = {}
@@ -89,102 +86,102 @@ time_dict = {}
 
 def callback(message):
     try:
-	body = message["data"]
-	output = json.loads(body)
-	vin = output["vin"]
+        body = message["data"]
+        output = json.loads(body)
+        vin = output["vin"]
 
-	# impute maf if missing
-	if output["maf_airflow"] == "":
-	    # engine displacement in l
-	    ed = 1.8
-	    # volumetric efficiency
-	    ve = 0.9
-	    imap = output["rpm"] * output["intake_manifold_pressure"] / (output["intake_air_temp"] + 273.15)
-	    # 28.97 = mass of air
-	    # 8.314 = gas constant
-	    output["maf_airflow"] = (imap/120.) * ve * ed * (28.97/8.314)
+        # impute maf if missing
+        if output["maf_airflow"] == "":
+            # engine displacement in l
+            ed = 1.8
+            # volumetric efficiency
+            ve = 0.9
+            imap = output["rpm"] * output["intake_manifold_pressure"] / (output["intake_air_temp"] + 273.15)
+            # 28.97 = mass of air
+            # 8.314 = gas constant
+            output["maf_airflow"] = (imap/120.) * ve * ed * (28.97/8.314)
 
-	# impute fuel level if missing
-	if output["fuel_level_input"] == "":  # TODO: take last value if it exists
-	    output["fuel_level_input"] = 50
+        # impute fuel level if missing
+        if output["fuel_level_input"] == "":  # TODO: take last value if it exists
+            output["fuel_level_input"] = 50
 
-	if vin in journeys:
-	    journey = journeys[vin]
-	    journey.load_streaming_data(body)
-	    time_dict[vin] = concat([time_dict[vin], Series(datetime.now())])
-	    if (journey.data["Time_GPS"].iloc[-1] - journey.data["Time_GPS"].iloc[-2]) > cutoff \
-	       or (time_dict[vin].iloc[-1] - time_dict[vin].iloc[-2]) > cutoff:
-		journey = Data.Journey()
-		journey.load_streaming_data(body)
-		journeys[vin] = journey
-	else:
-	    journey = Data.Journey()
-	    journey.load_streaming_data(body)
-	    journeys[vin] = journey
+        if vin in journeys:
+            journey = journeys[vin]
+            journey.load_streaming_data(body)
+            time_dict[vin] = concat([time_dict[vin], Series(datetime.now())])
+            if (journey.data["Time_GPS"].iloc[-1] - journey.data["Time_GPS"].iloc[-2]) > cutoff \
+               or (time_dict[vin].iloc[-1] - time_dict[vin].iloc[-2]) > cutoff:
+                journey = Data.Journey()
+                journey.load_streaming_data(body)
+                journeys[vin] = journey
+        else:
+            journey = Data.Journey()
+            journey.load_streaming_data(body)
+            journeys[vin] = journey
 
-	# create initial prediction:
-	counter = len(journey.data.index)
-	if counter == 1:
-	    initial_predictions[vin] = init_models[vin].predict(journey)
-	    prob_dict[vin] = initial_predictions[vin]
-	    time_dict[vin] = Series(datetime.now())
+        # create initial prediction:
+        counter = len(journey.data.index)
+        if counter == 1:
+            initial_predictions[vin] = init_models[vin].predict(journey)
+            prob_dict[vin] = initial_predictions[vin]
+            time_dict[vin] = Series(datetime.now())
 
-	# create online prediction:
-	online_prediction = online_models[vin].predict(journey)
+        # create online prediction:
+        online_prediction = online_models[vin].predict(journey)
 
-	# mix initial and online prediction
-	mixing_factor = min(counter/mixing_length, 1)
-	probabilities = (1 - mixing_factor) * initial_predictions[vin] + mixing_factor * online_prediction
+        # mix initial and online prediction
+        mixing_factor = min(counter/mixing_length, 1)
+        probabilities = (1 - mixing_factor) * initial_predictions[vin] + mixing_factor * online_prediction
 
-	if counter > 1:
-	    prob_dict[vin] = np.vstack((prob_dict[vin], probabilities))
+        if counter > 1:
+            prob_dict[vin] = np.vstack((prob_dict[vin], probabilities))
 
-	if counter > smoothing_length:
-	    smoothed_probabilities = np.mean(prob_dict[vin][(counter - smoothing_length + 1): (counter + 1), :], axis=0, keepdims=True)
-	else:
-	    smoothed_probabilities = probabilities
+        if counter > smoothing_length:
+            smoothed_probabilities = np.mean(prob_dict[vin][(counter - smoothing_length + 1): (counter + 1), :], axis=0, keepdims=True)
+        else:
+            smoothed_probabilities = probabilities
 
-	# calculate remaining range
-	mpg = np.nansum(smoothed_probabilities * mpg_insts[vin])
-	remaining_fuel_gallons = ((np.array(journey.data.FuelRemaining.tail(1), dtype="float64")/100 * float(fuel_capacity)) * 0.264172052)[0]
+        # calculate remaining range
+        mpg = np.nansum(smoothed_probabilities * mpg_insts[vin])
+        remaining_fuel_gallons = ((np.array(journey.data.FuelRemaining.tail(1), dtype="float64")/100 * float(fuel_capacity)) * 0.264172052)[0]
 
-	if (units == "miles"):
-	    remaining_range = int(remaining_fuel_gallons * mpg)
-	else:
-	    remaining_range = (remaining_fuel_gallons * mpg) * 1.609344
+        if (units == "miles"):
+            remaining_range = int(remaining_fuel_gallons * mpg)
+        else:
+            remaining_range = (remaining_fuel_gallons * mpg) * 1.609344
 
-	# create cluster information
-	container = zip(cluster_ids[vin], smoothed_probabilities[0], end_locations[vin], mpg_insts[vin])
-	cluster_predictions = dict()
-	for jid, prob, endloc, mpg in container:
-	    cluster_predictions[jid] = {"Probability": round(prob, 3), "EndLocation": endloc, "MPG_Journey": round(mpg, 5)}
+        # create cluster information
+        container = zip(cluster_ids[vin], smoothed_probabilities[0], end_locations[vin], mpg_insts[vin])
+        cluster_predictions = dict()
+        for jid, prob, endloc, mpg in container:
+            cluster_predictions[jid] = {"Probability": round(prob, 3), "EndLocation": endloc, "MPG_Journey": round(mpg, 5)}
 
-	# put together final dict
-	predictions = dict()
-	predictions["ClusterPredictions"] = cluster_predictions
-	predictions["RemainingRange"] = remaining_range
+        # put together final dict
+        predictions = dict()
+        predictions["ClusterPredictions"] = cluster_predictions
+        predictions["RemainingRange"] = remaining_range
 
-	output["Predictions"] = predictions
-	output["mpg_instantaneous"] = journey.data.MPG_from_MAF.tail(1).values[0]
-	if (units == "miles"):
-	    output["vehicle_speed"] = int(output["vehicle_speed"] * 0.621371)
+        output["Predictions"] = predictions
+        output["mpg_instantaneous"] = journey.data.MPG_from_MAF.tail(1).values[0]
+        if (units == "miles"):
+            output["vehicle_speed"] = int(output["vehicle_speed"] * 0.621371)
 
-	return json.dumps(output, ensure_ascii=False, encoding="utf-8")
+        return json.dumps(output, ensure_ascii=False, encoding="utf-8")
 
     except ValueError as e:
-	logging.exception("Got error %s with payload %s and output %s ", e, body, output)
+        logging.exception("Got error %s with payload %s and output %s ", e, body, output)
     except TypeError as e:
-	logging.exception("Got error %s with payload %s and output %s", e, body, output)
+        logging.exception("Got error %s with payload %s and output %s", e, body, output)
     except Exception as e:
-	logging.exception("Got error %s with payload %s and output %s", e, body, output)
+        logging.exception("Got error %s with payload %s and output %s", e, body, output)
 
 
 def main():
     p = r.pubsub(ignore_subscribe_messages=True)
     p.subscribe(raw_data_queue)
     for message in p.listen():
-	predictions = callback(message)
-	p.publish(predictions_topic, predictions)
+        predictions = callback(message)
+        p.publish(predictions_topic, predictions)
 
 
 if __name__ == "__main__":
